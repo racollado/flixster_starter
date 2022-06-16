7a29d199965e4f96597e4e8f949d58250e3f86aa
Rafael Collado <rco@fb.com>
2022-06-16 16:25:06 -0700

    detailed info pop-up

--- script.js
+++ script.js
@@ -7,11 +7,17 @@ var currentPage = 1
 // HTML QUERY SELECTORS AND GET ELEMENT CALLS
 
 const moreButtonElement = document.querySelector("#more-button");
-const searchElement = document.querySelector("#current-query")
-const clearElement = document.querySelector("#clear")
+const searchElement = document.querySelector("#current-query");
+const clearElement = document.querySelector("#clear");
 const moviesDiv = document.getElementById("movies");
-const posters = document.querySelector(".image")
-const titles = document.querySelector(".title");
+const dimScreen = document.getElementById("dim");
+const dimX = document.getElementById("x");
+const dimContent = document.getElementById("content");
+const dimTitle = document.getElementById("dim-title");
+const dimTag = document.getElementById("dim-tagline");
+const dimVid = document.getElementById("dim-video");
+const dimOver = document.getElementById("dim-overview");
+const dimElements = [dimScreen, dimX, dimContent, dimTitle, dimTag, dimOver, dimVid];
 
 // MOVIE FETCHING FUNCTIONS
 
@@ -46,18 +52,33 @@ function getMovies() {
     }
 }
 
+async function getMovieInfo(movieID) {
+    try { 
+        let results = await fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=804e54f236cc43faf2e40adb1019cf62&language=en-US`);
+        let data = await results.json();
+        if (data.video) {
+           results = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}/videos?api_key=804e54f236cc43faf2e40adb1019cf62&language=en-US`);
+           data += await results.json();
+        }
+        console.log(data);
+        return data;
+    } catch(err) {
+        console.error(err);
+    }
+}
+
 // HTML MOVIE CARD RENDERING FUNCTION
 
 function renderMovieCard(div, movieObj) {
     div.innerHTML += `
     <div class="movie-card item">
-    <div class="image">
+    <div class="image" onclick="movieClicked('${movieObj.id}')">
     ${movieObj.poster_path === null ? 
         `<h2 class="no-image" style="color: red">[No Image Found]</h2>` : 
         `<img class="movie-poster-image" src=` + baseImgURL + movieObj.poster_path + ` alt="movie poster image" />`}
     </div>
     <br>
-    <h2 class="title">Title: ${movieObj.title}</h2>
+    <h2 class="title" onclick="movieClicked('${movieObj.id}')">Title: ${movieObj.title}</h2>
     <br>
     <h3>Votes: ${movieObj.vote_average}</h3>
     </div>
@@ -82,6 +103,23 @@ function updateCurrentQuery() {
 
 function clearMovies(div) {
     div.innerHTML = ``;
+}
+
+function movieClicked(movieID) {
+    console.log(movieID);
+    openDim(movieID);
+}
+
+async function openDim(movieID) {
+    data = await getMovieInfo(movieID);
+    dimTitle.innerHTML = `${data.title} (${data.release_date.substring(0,4)})`;
+    dimTag.innerHTML = data.tagline;
+    dimOver.innerHTML = data.overview;
+    dimElements.forEach(el => {el.classList.add("dim-screen");});
+}
+
+function closeDim() {
+    dimElements.forEach(el => {el.classList.remove("dim-screen");});
 }
 
 // ADD EVENT LISTENERS
@@ -116,11 +154,6 @@ function attachEventListeners() {
         for (const index in movieData.results) {
             renderMovieCard(moviesDiv, movieData.results[index]);
         }});
-
-    // click movie or title to open pop-up
-    posters.addEventListener('click', () => {console.log('hey')})
-
-    titles.addEventListener('click', () => {console.log('bitch')})
 }
 
 // WINDOW ONLOAD CALLS
